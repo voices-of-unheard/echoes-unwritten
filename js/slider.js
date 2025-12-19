efd2400ff813df573b0ca76622a2ddca25f2f67b
voices-of-unheard <voicesof.unheard2025@gmail.com>
2025-12-20 05:17:18 +0530

    slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,9 @@
-// Future use:
-// - Fetch stories from backend
-// - Control speed
-// - Pause on hover
+function setTheme(theme) {
+  document.body.className = theme;
+  localStorage.setItem("site-theme", theme);
+}
+
+window.onload = () => {
+  const saved = localStorage.getItem("site-theme") || "theme-dark";
+  document.body.className = saved;
+};
