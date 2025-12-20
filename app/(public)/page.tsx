a7234b2c260550e46771eaacff9112cddd2a57a4
voices-of-unheard <voicesof.unheard2025@gmail.com>
2025-12-20 05:50:11 +0530

    Create page.tsx

---
+++ app/(public)/page.tsx
@@ -0,0 +1,9 @@
+import Trending from "@/components/discovery/Trending";
+
+export default function HomePage() {
+  return (
+    <main className="max-w-7xl mx-auto p-6">
+      <Trending />
+    </main>
+  );
+}
