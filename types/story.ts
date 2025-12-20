191f9f771f570d38371db9e8eb861a868849a4bc
voices-of-unheard <voicesof.unheard2025@gmail.com>
2025-12-20 05:47:40 +0530

    Create story.ts

---
+++ types/story.ts
@@ -0,0 +1,12 @@
+export interface Story {
+  id: string;
+  title: string;
+  content: string;
+  preview: string;
+  category: string;
+  emotion: string;
+  anonymous: boolean;
+  views: number;
+  likes: number;
+  created_at: string;
+}
