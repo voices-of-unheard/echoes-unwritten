2e5080ea1578efc06fbd85cd28f4f4d66fbad514
voices-of-unheard <voicesof.unheard2025@gmail.com>
2025-12-20 23:24:52 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL=https://hnvtszvzipiavwoqvxgo.supabase.co;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhudnRzenZ6aXBpYXZ3b3F2eGdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxNzI2NTksImV4cCI6MjA4MTc0ODY1OX0.SlrDpNujc0r1yZEc205LFkNcbHJb-VzNug5LWxGOFS0;
 
 export const supabase = createClient(
   supabaseUrl,
