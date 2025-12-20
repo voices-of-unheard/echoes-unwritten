319678ecfb57dac9e4811510e7b274d0e0de5f79
voices-of-unheard <voicesof.unheard2025@gmail.com>
2025-12-20 05:43:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-function toggleMenu() {
-  const menu = document.getElementById("sideMenu");
-  if (menu.style.left === "0px") {
-    menu.style.left = "-250px";
-  } else {
-    menu.style.left = "0px";
-  }
-}
+
